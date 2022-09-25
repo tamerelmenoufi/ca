7182b19a3a451f476cd0edf81806081a9ec33591
tamerelmenoufi <tamer@mohatron.com.br>
2022-09-25 17:26:41 -0400

    Novo Projeto

--- src/home/index.php
+++ src/home/index.php
@@ -11,18 +11,16 @@
         </div>
     </div>
 
-    <div class="row">
-        <div class="card m-3">
-            <div class="row">
-                <div class="col-md-6 text-left">
-                    <img src="img/capa.jpg" class="img-fluid rounded-start">
-                </div>
-                <div class="col-md-6">
-                    <div class="card-body">
-                        <h5 class="card-title">Card title</h5>
-                        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                        <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-                    </div>
+    <div class="card m-3">
+        <div class="row">
+            <div class="col-md-6 text-left">
+                <img src="img/capa.jpg" class="img-fluid rounded-start">
+            </div>
+            <div class="col-md-6">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                    <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
                 </div>
             </div>
         </div>
