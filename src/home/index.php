0f7509ed33869b62deb07f6236ed0958a4e9b01c
tamerelmenoufi <tamer@mohatron.com.br>
2022-09-25 17:43:46 -0400

    Novo Projeto

--- src/home/index.php
+++ src/home/index.php
@@ -38,12 +38,25 @@
                 </div>
             </div>
             <div class="col-md-6" style="text-align:right;">
-                <img src="img/capa1.jpg" class="img-fluid rounded-end">
+                <img src="img/proposta.jpg" class="img-fluid rounded-end">
             </div>
         </div>
     </div>
 
-
+    <div class="card mt-3">
+        <div class="row">
+            <div class="col-md-6 text-left">
+                <img src="img/capa1.jpg" class="img-fluid rounded-start">
+            </div>
+            <div class="col-md-6">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                    <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                </div>
+            </div>
+        </div>
+    </div>
 
 </div>
 
