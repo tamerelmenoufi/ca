0bc7e18fab46d6ebd6f4f43566edfa761163680a
tamerelmenoufi <tamer@mohatron.com.br>
2022-09-25 16:38:51 -0400

    Novo Projeto

--- src/home/index.php
+++ src/home/index.php
@@ -1,4 +1,13 @@
 Conteúdo da HOME
+<div class="container">
+    <div class="row">
+        <div class="col-offset-1 col-md-10">
+            <img src="img/banner.jpg" />
+        </div>
+    </div>
+</div>
+
+
 
 <script>
     $(function(){
