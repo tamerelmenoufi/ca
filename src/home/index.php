9769b873c659b96b318c20f19aa0c8f003060747
tamerelmenoufi <tamer@mohatron.com.br>
2022-09-25 17:09:03 -0400

    Novo Projeto

--- src/home/index.php
+++ src/home/index.php
@@ -1,4 +1,9 @@
 <div class="container">
+
+    <nav class="navbar" style="background-color: #e3f2fd;">
+    <!-- Navbar content -->
+    </nav>
+
     <div class="row">
         <div class="col-md-12 text-center">
             <img src="img/banner.jpg" class="img-fluid" />
