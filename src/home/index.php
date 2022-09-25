ce73d71f78a3620c8207f9dceff8d8132e66eeea
tamerelmenoufi <tamer@mohatron.com.br>
2022-09-25 17:19:42 -0400

    Novo Projeto

--- src/home/index.php
+++ src/home/index.php
@@ -1,6 +1,3 @@
-
-
-
 <nav class="navbar fixed-top bg-light">
   <div class="container-fluid">
     <a class="navbar-brand" href="#">Fixed top</a>
@@ -13,6 +10,24 @@
             <img src="img/banner.jpg" class="img-fluid" />
         </div>
     </div>
+
+    <div class="row">
+        <div class="card mb-3">
+            <div class="row g-0">
+                <div class="col-md-4">
+                    <img src="img/capa.jpg" class="img-fluid rounded-start">
+                </div>
+                <div class="col-md-8">
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                        <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
 </div>
 
 
