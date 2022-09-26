61fdf027779da06bc19b38660fcde2e2226874b0
tamerelmenoufi <tamer@mohatron.com.br>
2022-09-25 22:53:16 -0400

    Novo Projeto

--- src/home/index.php
+++ src/home/index.php
@@ -88,8 +88,8 @@
 <nav class="navbar fixed-bottom bg-light">
   <div class="container-fluid">
     <button class="btn btn-success btn-lg" style="position:relative;">
-        <span style="margin-left:50px;">Coronel Amadeu 35111</span>
-        <i class="fa-regular fa-hand-peace" style="position:absolute; left:5px; top:5px; font-size:40px;"></i>
+        <span style="margin-left:40px;">Coronel Amadeu 35111</span>
+        <i class="fa-regular fa-hand-peace" style="position:absolute; left:5px; top:2px; font-size:40px;"></i>
     </button>
   </div>
 </nav>
