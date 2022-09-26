8d712382490f5c1e961e9b4b58cdc436080a0c66
tamerelmenoufi <tamer@mohatron.com.br>
2022-09-25 23:34:25 -0400

    Novo Projeto

--- index.php
+++ index.php
@@ -38,7 +38,7 @@
             });
 
             $.alert({
-              content:'<center>Respeitamos a sua privacidade e por isso que seguimos a lei geral de proteção de dados LGPD.<br><br>Se não quiser receber mais nossas notificações, clique no botão <button class="btn btn-link btn-sm" style="color:red; text-decoration:none"><i class="fa-regular fa-circle-xmark"></i> Sair</button> no rodapé desta página.</center>',
+              content:'<center>Respeitamos a sua privacidade, seguindo as normas da lei geral de proteção de dados LGPD, Lei n° 13.709/2018.<br><br>Se não quiser receber mais nossas notificações, clique no botão <button class="btn btn-link btn-sm" style="color:red; text-decoration:none"><i class="fa-regular fa-circle-xmark"></i> Sair</button> no rodapé desta página e o seu cadastro será desativado imediatamente.</center>',
               title:false,
               style:'red'
             })
