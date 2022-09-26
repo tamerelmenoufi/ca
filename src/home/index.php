9f6e21a068515aa27f523217b368b268bc90f572
tamerelmenoufi <tamer@mohatron.com.br>
2022-09-25 23:16:11 -0400

    Novo Projeto

--- src/home/index.php
+++ src/home/index.php
@@ -76,12 +76,12 @@
                 <div class="card-body">
                     <h5 class="card-title">Card title</h5>
                     <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                    <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
                     <p>
                     <audio controls loop class="somX" style="width:100%">
                         <source src="audio/som.mp3" type="audio/mpeg">
                     </audio>
                     </p>
+                    <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
                 </div>
             </div>
         </div>
