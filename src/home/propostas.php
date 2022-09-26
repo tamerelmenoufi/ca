4382153acd7c30897de046687b235e0414bf7cbb
tamerelmenoufi <tamer@mohatron.com.br>
2022-09-25 20:53:42 -0400

    Novo Projeto

--- src/home/propostas.php
+++ src/home/propostas.php
@@ -9,21 +9,21 @@
                 </div>
                 <div class="carousel-inner">
                     <div class="carousel-item active">
-                    <img src="img/1a3.jpg"  >
+                    <img src="img/1a3.jpg" style="height:100%"  >
                     <div class="carousel-caption d-none d-md-block">
                         <h5>First slide label</h5>
                         <p>Some representative placeholder content for the first slide.</p>
                     </div>
                     </div>
                     <div class="carousel-item">
-                    <img src="img/1a3.jpg" >
+                    <img src="img/1a3.jpg" style="height:100%" >
                     <div class="carousel-caption d-none d-md-block">
                         <h5>Second slide label</h5>
                         <p>Some representative placeholder content for the second slide.</p>
                     </div>
                     </div>
                     <div class="carousel-item">
-                    <img src="img/1a3.jpg" >
+                    <img src="img/1a3.jpg" style="height:100%" >
                     <div class="carousel-caption d-none d-md-block">
                         <h5>Third slide label</h5>
                         <p>Some representative placeholder content for the third slide.</p>
