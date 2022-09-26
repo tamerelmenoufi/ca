7059f080445ab23ceee4fee3b9b226750dd55f7a
tamerelmenoufi <tamer@mohatron.com.br>
2022-09-26 09:04:37 -0400

    Novo Projeto

--- index.php
+++ index.php
@@ -23,6 +23,23 @@
     <div class="popup"></div>
 
 
+    <div class="container "
+    style=" margin:0px;position:fixed!important; bottom:0;
+    background-color:#000000d6;z-index:999999999999;padding:20px;width:100%;color:#fff;font-weight:bold"
+    id="privacidade"
+    >
+        <div class="col-md-9" style="" >
+            Este site utiliza cookies confiáveis e inofensivos para garantir uma melhor experiência de navegação. <br>
+            <a style="font-size:16px;" href="#" abrir="src/home/privacidade.php">Política de Privacidade. </a>
+        </div>
+
+        <div class="col-md-3" style="" >
+            <span><a id="ocultar" style="border-radius:16px;margin:5px;font-size:16px;" class="btn btn-warning pull-right"  role="button">Aceitar</a></span>
+        </div>
+    </div>
+
+
+
     <?php
     include("lib/footer.php");
     ?>
@@ -41,6 +58,19 @@
               content:'<center>Respeitamos a sua privacidade, seguindo as normas da lei geral de proteção de dados LGPD, Lei n° 13.709/2018.<br><br>Se não quiser receber mais nossas notificações, clique no botão <button class="btn btn-link btn-sm" style="color:red; text-decoration:none"><i class="fa-regular fa-circle-xmark"></i> Sair</button> no rodapé desta página e o seu cadastro será desativado imediatamente.</center>',
               title:false,
               style:'red'
+            })
+
+            $("#ocultar").click(function(){
+              $("#privacidade").remove();
+            });
+
+            $("a[abrir]").click(function(){
+              local = $(this).attr("abrir");
+              $.dialog({
+                content:"url:"+local,
+                title:"Política de Privacidade",
+                columnClass:'col-md-12'
+              })
             })
 
         })
