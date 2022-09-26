792a8d360d9f6b1d5fa60fba201aa9f742ead1ab
tamerelmenoufi <tamer@mohatron.com.br>
2022-09-26 09:42:59 -0400

    Novo Projeto

--- index.php
+++ index.php
@@ -59,7 +59,7 @@
             });
 
             $.alert({
-              content:'<center>Respeitamos a sua privacidade, seguindo as normas da lei geral de proteção de dados LGPD, Lei n° 13.709/2018.<br><br>Se não quiser receber mais nossas notificações, clique no botão <button class="btn btn-link btn-sm" style="color:red; text-decoration:none"><i class="fa-regular fa-circle-xmark"></i> Sair</button> no rodapé desta página e o seu cadastro será desativado imediatamente.</center>',
+              content:'<center>Respeitamos a sua privacidade, seguindo as normas da lei geral de proteção de dados LGPD, Lei n° 13.709/2018.<br><br>Se não quiser receber mais nossas notificações, clique no botão <button class="btn btn-link btn-sm" style="color:green; text-decoration:none"><i class="fa-solid fa-lock"></i> Privacidade</button> no rodapé desta página e o seu cadastro será desativado imediatamente.</center>',
               title:false,
               style:'red'
             })
--- src/home/index.php
+++ src/home/index.php
@@ -100,9 +100,9 @@
         <i class="fa-regular fa-hand-peace" style="position:absolute; left:5px; top:2px; font-size:30px;"></i>
     </button>
 
-    <button class="btn btn-link btn-sm" style="color:red; text-decoration:none">
-        <i class="fa-regular fa-circle-xmark"></i>
-        Sair
+    <button class="btn btn-link btn-sm" style="color:green; text-decoration:none">
+        <i class="fa-solid fa-lock"></i>
+        Privacidade
     </button>
   </div>
 </nav>
