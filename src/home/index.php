d7215763c6e540533644e0a0ac1cd207c7993c02
tamerelmenoufi <tamer@mohatron.com.br>
2022-09-25 23:40:56 -0400

    Novo Projeto

--- src/home/index.php
+++ src/home/index.php
@@ -1,7 +1,10 @@
 <nav class="navbar fixed-top bg-light">
   <div class="container-fluid">
     <img src="img/icone.png" style="position:absolute; top:10px; left:20px; border:2px solid #eee; border-radius:100%; width:60px;" >
-    <a class="navbar-brand" href="#" style="margin-left:80px;">CORONEL AMADEU</a>
+    <a class="navbar-brand" href="#" style="margin-left:80px;">
+        CORONEL AMADEU
+        <i class="fa-solid fa-volume-high" style="margin-left:15px; cursor:point;"></i>
+    </a>
   </div>
 </nav>
 
