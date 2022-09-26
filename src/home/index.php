b958d9b4d7a57b70ba8edcc54aeb6dac482f78ab
tamerelmenoufi <tamer@mohatron.com.br>
2022-09-25 20:13:44 -0400

    Novo Projeto

--- src/home/index.php
+++ src/home/index.php
@@ -21,6 +21,28 @@
                 <div class="card-body">
                     <h5 class="card-title">Card title</h5>
                     <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                    <div class="row">
+                        <div class="col-md-4">
+                            <img src="img/1a3.jpg" class="img-fluid" />
+                        </div>
+                        <div class="col-md-4">
+                            <img src="img/1a3.jpg" class="img-fluid" />
+                        </div>
+                        <div class="col-md-4">
+                            <img src="img/1a3.jpg" class="img-fluid" />
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-4">
+                            <img src="img/1a3.jpg" class="img-fluid" />
+                        </div>
+                        <div class="col-md-4">
+                            <img src="img/1a3.jpg" class="img-fluid" />
+                        </div>
+                        <div class="col-md-4">
+                            <img src="img/1a3.jpg" class="img-fluid" />
+                        </div>
+                    </div>
                     <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
                 </div>
             </div>
