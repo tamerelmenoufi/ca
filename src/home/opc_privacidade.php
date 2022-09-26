39b0c26ed1feafcc36e4c9a0865e4b4279259cb3
tamerelmenoufi <tamer@mohatron.com.br>
2022-09-26 10:25:24 -0400

    Novo Projeto

--- src/home/opc_privacidade.php
+++ src/home/opc_privacidade.php
@@ -1,10 +1,10 @@
 <div class="col-md-12">
     <button class="btn btn-primary mt-3 btn-lg w-100">
         <i class="fa-solid fa-spell-check"></i>
-        POLÍTICA DE PRIVACIDADE
+        Política de Privacidade
     </button>
     <button class="btn btn-danger mt-3 btn-lg w-100">
         <i class="fa-solid fa-ban"></i>
-        SOLICITAR BLOQUEIO
+        Solicitar Bloqueio
     </button>
 </div>
