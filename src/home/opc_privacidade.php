9d943b9bce42c1602146053aa6be2eb58f66cb1e
tamerelmenoufi <tamer@mohatron.com.br>
2022-09-26 10:31:41 -0400

    Novo Projeto

--- src/home/opc_privacidade.php
+++ src/home/opc_privacidade.php
@@ -1,10 +1,39 @@
 <div class="col-md-12 mt-3">
-    <button class="btn btn-primary mt-3 btn-lg w-100">
+    <button politica class="btn btn-primary mt-3 btn-lg w-100">
         <i class="fa-solid fa-spell-check"></i>
         Política de Privacidade
     </button>
-    <button class="btn btn-danger mt-3 btn-lg w-100">
+    <button bloqueio class="btn btn-danger mt-3 btn-lg w-100">
         <i class="fa-solid fa-ban"></i>
         Solicitar Bloqueio
     </button>
 </div>
+
+<script>
+    $(function(){
+
+        $("button[politica]").click(function(){
+            $.dialog({
+            content:"url:src/home/privacidade.php",
+            title:"Política de Privacidade",
+            columnClass:'col-md-12'
+            })
+        })
+
+        $("button[bloqueio]").click(function(){
+            $.confirm({
+                content:"Deseja realmente bloquear o nosso contato com você?",
+                titule:false,
+                buttons:{
+                    'SIM':function(){
+                        $.alert('Seus dados foram bloqueados de nossa base!');
+                    },
+                    'NÃO':function(){
+
+                    }
+                }
+            })
+        });
+
+    })
+</script>
