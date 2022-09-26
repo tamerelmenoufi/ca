9a17db9dc37dc8fb55261273e09e14acb2bea813
tamerelmenoufi <tamer@mohatron.com.br>
2022-09-25 20:58:57 -0400

    Novo Projeto

--- src/home/propostas.php
+++ src/home/propostas.php
@@ -8,26 +8,48 @@
                     <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
                 </div>
                 <div class="carousel-inner">
+
                     <div class="carousel-item active">
-                    <img src="img/1a3.jpg" style="height:100%"  >
-                    <div class="carousel-caption d-none d-md-block">
-                        <h5>First slide label</h5>
-                        <p>Some representative placeholder content for the first slide.</p>
+                        <div class="row">
+                            <div class="col-md-6">
+                                <img src="img/1a3.jpg" class="d-block w-100" >
+                            </div>
+                            <div class="col-md-6">
+                                <div class="carousel-caption d-none d-md-block">
+                                    <h5>First slide label</h5>
+                                    <p>Some representative placeholder content for the first slide.</p>
+                                </div>
+                            </div>
+                        </div>
                     </div>
-                    </div>
+
+
                     <div class="carousel-item">
-                    <img src="img/1a3.jpg" style="height:100%" >
-                    <div class="carousel-caption d-none d-md-block">
-                        <h5>Second slide label</h5>
-                        <p>Some representative placeholder content for the second slide.</p>
+                        <div class="row">
+                            <div class="col-md-6">
+                                <img src="img/1a3.jpg" class="d-block w-100" >
+                            </div>
+                            <div class="col-md-6">
+                                <div class="carousel-caption d-none d-md-block">
+                                    <h5>First slide label</h5>
+                                    <p>Some representative placeholder content for the first slide.</p>
+                                </div>
+                            </div>
+                        </div>
                     </div>
-                    </div>
+
                     <div class="carousel-item">
-                    <img src="img/1a3.jpg" style="height:100%" >
-                    <div class="carousel-caption d-none d-md-block">
-                        <h5>Third slide label</h5>
-                        <p>Some representative placeholder content for the third slide.</p>
-                    </div>
+                        <div class="row">
+                            <div class="col-md-6">
+                                <img src="img/1a3.jpg" class="d-block w-100" >
+                            </div>
+                            <div class="col-md-6">
+                                <div class="carousel-caption d-none d-md-block">
+                                    <h5>First slide label</h5>
+                                    <p>Some representative placeholder content for the first slide.</p>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
                 <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
